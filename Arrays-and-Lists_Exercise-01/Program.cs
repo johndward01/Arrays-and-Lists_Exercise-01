3f1d3fd9e682e750673b01ea4c43c8b217732303
johndward01 <johndward01@gmail.com>
2021-08-11 12:25:01 -0500

    Add project files.

---
+++ Arrays-and-Lists_Exercise-01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arrays_and_Lists_Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
